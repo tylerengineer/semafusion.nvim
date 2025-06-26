3c032e2d15f15fc76389b026e60eef9a252e0b6a
tylerengineer <tyler.engineer@hotmail.com>
2025-06-26 12:43:43 -0400

    Created colors.lua

---
+++ lua/semafusion/colors.lua
@@ -0,0 +1,44 @@
+-- lua/semafusion/colors.lua
+
+return {
+    vibrant = {
+        light = {
+            bg = '#f8f8f7',
+            fg = '#151515',
+            accent = '#1dcd76',
+            surface = '#ffffff',
+            subtle = '#e0e0de',
+            border = '#cccccc',
+            headline = '#222222',
+        },
+        dark = {
+            bg = '#1f1f1e',
+            fg = '#f5f5f4',
+            accent = '#1dcd76',
+            surface = '#2b2b2b',
+            subtle = '#3c3c3b',
+            border = '#4a4a49',
+            headline = '#ffffff',
+        },
+    },
+    muted = {
+        light = {
+            bg = '#f3f2f1',
+            fg = '#2c2c2c',
+            accent = '#1dcd76',
+            surface = '#ffffff',
+            subtle = '#dcdcdc',
+            border = '#bbbbbb',
+            headline = '#333333',
+        },
+        dark = {
+            bg = '#252524',
+            fg = '#eaeaea',
+            accent = '#1dcd76',
+            surface = '#323231',
+            subtle = '#424241',
+            border = '#545453',
+            headline = '#fefefe',
+        },
+    },
+}
