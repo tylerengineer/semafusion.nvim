c40b357aed135ce7803393e81defe40bde9db9df
tylerengineer <tyler.engineer@hotmail.com>
2025-06-26 13:09:44 -0400

    Updated inner init.lua

--- lua/semafusion/init.lua
+++ lua/semafusion/init.lua
@@ -4,13 +4,16 @@ local M = {}
 
 M.load = function(opts)
     opts = opts or {}
-    local variant = opts.variant or 'dark'
-    local style = opts.style or 'vibrant'
+    local variant = opts.variant or 'light'
+    local style = opts.style or 'editorial'
     local transparent = opts.transparent or false
 
-    local colors = require('semafusion.colors')[style]
+    local colors = require('semafusion.colors').get(variant, style)
     local theme = require('semafusion.' .. variant)
-    theme.setup(colors[variant], transparent)
+    theme.setup(colors, transparent)
+
+    vim.o.background = variant
+    vim.g.colors_name = 'semafusion-' .. variant
 end
 
 return M
